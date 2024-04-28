1bbde2be0f26190e39f15c0348df8065405d16e2
nerusaur <146534280+nerusaur@users.noreply.github.com>
2024-04-28 16:12:02 +0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library;
+
+/**
+ *
+ * @author NEIL
+ */
+public class Book {
+    
+ public Book(String tittle, String author) {
+        
+        this.tittle = tittle;
+        this.author = author;
+    }
+  
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    private String name;
+    private String tittle, author;
+
+   
+}
